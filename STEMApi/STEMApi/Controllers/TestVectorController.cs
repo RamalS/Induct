d94bb5526ee2afb5e5b67f691bc1204f30a889cd
kiletic <kiletic@gmail.com>
2023-05-12 11:59:33 +0200

    Started working on generating test vectors

---
+++ STEMApi/STEMApi/Controllers/TestVectorController.cs
@@ -0,0 +1,38 @@
+using Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace STEMApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestVectorController : ControllerBase
+    {
+        private readonly IInputCondition IInputCondition;
+        private readonly ISample ISample;
+
+        public TestVectorController(IInputCondition iInputCondition, ISample iSample)
+        {
+            IInputCondition = iInputCondition;
+            ISample = iSample;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll(List<TestInputCollection> testInputCollection)
+        {
+            List<TestVector> testVectors = new List<TestVector>();
+            
+            foreach (Sample sample in ISample.GetAll()) {
+              List<TestInputCollection> filteredCollection = testInputCollection.Where(x => x.SampleIds.Contains(sample.Id)).ToList();
+              foreach(TestInputCollection coll in filteredCollection)
+              {
+                foreach(TestInputCollection coll2 in filteredCollection.Where(x => x.Id != coll.Id))
+              }
+            }
+
+
+            return Ok(testVectors);
+        }
+    }
+}
