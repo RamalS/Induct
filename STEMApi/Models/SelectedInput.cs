472493b7f9159b6caeb59a662939beec8ebc55ff
Zoltan Palinkaš <zoltan.palinkas1@gmail.com>
2023-05-12 11:20:39 +0200

    added TestVector model

---
+++ STEMApi/Models/SelectedInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class SelectedInput
+    {
+        public int Id { get; set; }
+        public int InputConditionId { get; set; }
+        public double Value { get; set; }
+    }
+}
---
+++ STEMApi/Models/TestVector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class TestVector
+    {
+        public int Id { get; set; }
+        public bool IsUsed { get; set; }
+        public List<SelectedInput> SelectedInput { get; set; } = new List<SelectedInput>();
+    }
+}
